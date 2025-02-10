720c5df44c1d512b044c0c62a8d39f9ed6600c54
nook_owner <nook.dog871@gmail.com>
2025-02-10 14:42:52 +0000

    re-installed svelte and drizzle settings

--- drizzle/0000_tricky_devos.sql
+++ drizzle/0000_tricky_devos.sql
@@ -2,7 +2,7 @@ CREATE TABLE `user` (
 	`id` varchar(25) NOT NULL,
 	`name` text,
 	`role` text,
-	`created_at` datetime DEFAULT '2025-02-10 14:11:49.927',
-	`updated_at` datetime DEFAULT '2025-02-10 14:11:49.927',
+	`created_at` datetime DEFAULT '2025-02-10 14:41:39.054',
+	`updated_at` datetime DEFAULT '2025-02-10 14:41:39.054',
 	CONSTRAINT `user_id` PRIMARY KEY(`id`)
 );
