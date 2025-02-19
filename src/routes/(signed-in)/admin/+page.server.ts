5d2a293eb77a209632809e42e3caeb1ca2b8817f
ShunsukeHanai <sanza0919@gmail.com>
2025-02-19 13:50:05 +0000

    test

--- src/routes/(signed-in)/admin/+page.server.ts
+++ src/routes/(signed-in)/admin/+page.server.ts
@@ -4,9 +4,7 @@ import { clerkClient } from "svelte-clerk/server";
 export const load = async ({ locals }) => {
 	locals.auth.sessionClaims?.metadata?.role;
 	const userId = approveAdmin(locals.auth);
-
 	const user = await clerkClient.users.getUser(userId);
-
 	return {
 		userId: user.firstName,
 	};
